06a2d1b5a36ffd8b9b4a502791ca274bc2cf30a0
rankoe <rankoedev@gmail.com>
2023-11-18 13:23:55 +0100

    feat: add character

--- scenes/pg_char.gd
+++ scenes/pg_char.gd
@@ -1,38 +1,28 @@
 extends CharacterBody2D
 
 
-const SPEED = 400.0
-const JUMP_VELOCITY = -900.0
-@onready var frog = $frog
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	# Animations
-	if (velocity.x > 1 or velocity.x < -1):
-		frog.animation = "running"
-	else:
-		frog.animation = "default"
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		frog.animation = "jumping"
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("left", "right")
+	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, 25)
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-	
-	var isLeft = velocity.x <0
-	frog.flip_h = isLeft
